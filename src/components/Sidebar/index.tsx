d0108c59178207414efef1a241290eaa60d1e14f
rayague <rayague03@gmail.com>
2024-12-28 23:55:54 +0100

    correction pages

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -123,7 +123,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       >
         {/* SIDEBAR HEADER */}
         <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5 xl:py-10">
-          <Link href="/dashboard">
+          <Link href="/dashboardAgents">
             <Image
               width={176}
               height={32}
